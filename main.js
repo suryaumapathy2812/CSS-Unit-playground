58dce168fe059c9f678709a47974bb7c999b7e82
Surya Umapathy <suryaumapathy2812@gmail.com>
2022-11-07 11:09:21 +0530

    Added Relative Units

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 const abs_unit_section = document.getElementById("unit-absolute");
 const abs_inputs = Array.from(abs_unit_section.getElementsByClassName("unit-input"))
-console.log(abs_inputs)
 
 abs_inputs.forEach(function (input) {
 
@@ -17,3 +16,61 @@ abs_inputs.forEach(function (input) {
     })
 
 })
+
+
+const rel_unit_section = document.getElementById("unit-relative");
+const rel_inputs = Array.from(rel_unit_section.getElementsByClassName("unit-input"));
+
+const parent_inputs = [];
+const child_inputs = [];
+
+rel_inputs.forEach(function (input) {
+    if (input.dataset.level === "parent") {
+        parent_inputs.push(input);
+    } else {
+        child_inputs.push(input);
+    }
+})
+
+console.log(parent_inputs)
+console.log(child_inputs)
+
+
+parent_inputs.forEach(function (input) {
+    input.addEventListener("change", function (event) {
+        const input_target = event.target.dataset.target;
+        const target = Array.from(rel_unit_section.getElementsByClassName(input_target))[0];
+        const font_size = event.target.value;
+        const unit = input_target.split("-")[2]
+        console.log(target);
+        console.log(font_size);
+        console.log(unit);
+
+        target.style.fontSize = font_size + unit;
+    })
+})
+
+child_inputs.forEach(function (input) {
+    input.addEventListener("change", function (event) {
+        const input_target = event.target.dataset.target;
+        const target = Array.from(rel_unit_section.getElementsByClassName(input_target))[0];
+        const font_size = event.target.value;
+        const unit = input_target.split("-")[2]
+        console.log(target);
+        console.log(font_size);
+        console.log(unit);
+
+        target.style.fontSize = font_size + unit;
+    })
+})
+
+var viewPortWidth;
+var viewPortHeight;
+
+if (typeof window.innerWidth != 'undefined') {
+    viewPortWidth = window.innerWidth,
+        viewPortHeight = window.innerHeight
+}
+
+console.log("Viewport Weight ==> " + viewPortWidth)
+console.log("Viewport Height ==> " + viewPortHeight)
